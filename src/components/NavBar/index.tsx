462caf44dbe40249c1bd8e010631b9533b264519
Thomas MASIN (SAN DE RGOCC) <56608891+caerroff@users.noreply.github.com>
2024-05-27 17:43:57 +0200

    feat: added base layout + tailwindcss

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div className="px-5 py-2 border-2 border-gray-600 rounded-b-xl w-full">
+      <h1 className="text-start">Todo App</h1>
+    </div>
+  );
+}
+
+export default NavBar;
